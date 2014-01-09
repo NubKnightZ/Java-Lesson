cb5cde1a7ef79eb9f728347ca144b792c5b2ba0e
NubKnightZ <Kevin_Le_1996@yahoo.com>
2014-01-08 21:59:43 -0500

    Revert "Game Deck"
    This reverts commit adcdb7ef133e8a02fac6dee56ac1fa0fc505665e.

--- RandList.java
+++ RandList.java
@@ -10,157 +10,27 @@ public class RandList{
   
   public void Initializer(){
     ListName = new ArrayList<String>();
-    ListName.add("Kong Bucks");
-    ListName.add("10 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("10 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("10 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Kong Bucks");
-    ListName.add("2.5 Trillion");
-    ListName.add("Black Sun");
-    ListName.add("Treat this card as any card in the discard pile OR any Code Block for this turn only.");
-    ListName.add("Motorcycle");
-    ListName.add("When you move this turn, you may roll two dice instead. If the total rolled is 7 or higher, move that many steps instead of your normal move. Otherwise, move half the amount rolled. (Round Up)");
-    ListName.add("Motorcycle");
-    ListName.add("When you move this turn, you may roll two dice instead. If the total rolled is 7 or higher, move that many steps instead of your normal move. Otherwise, move half the amount rolled. (Round Up)");
-    ListName.add("Motorcycle");
-    ListName.add("When you move this turn, you may roll two dice instead. If the total rolled is 7 or higher, move that many steps instead of your normal move. Otherwise, move half the amount rolled. (Round Up)");
-    ListName.add("Motorcycle");
-    ListName.add("When you move this turn, you may roll two dice instead. If the total rolled is 7 or higher, move that many steps instead of your normal move. Otherwise, move half the amount rolled. (Round Up)");
-    ListName.add("Motorcycle");
-    ListName.add("When you move this turn, you may roll two dice instead. If the total rolled is 7 or higher, move that many steps instead of your normal move. Otherwise, move half the amount rolled. (Round Up)");
-    ListName.add("Juanita");
-    ListName.add("When this card is played, you may decrease a cooldown timer by 1.");
-    ListName.add("Juanita");
-    ListName.add("When this card is played, you may decrease a cooldown timer by 1.");
-    ListName.add("Juanita");
-    ListName.add("When this card is played, you may decrease a cooldown timer by 1.");
-    ListName.add("Juanita");
-    ListName.add("When this card is played, you may decrease a cooldown timer by 1.");
-    ListName.add("Juanita");
-    ListName.add("When this card is played, you may decrease a cooldown timer by 1.");
-    ListName.add("Rat");
-    ListName.add("Choose up to one player within 8 blocks of your character. That player may not make any movement during their next turn.");
-    ListName.add("Rat");
-    ListName.add("Choose up to one player within 8 blocks of your character. That player may not make any movement during their next turn.");
-    ListName.add("Rat");
-    ListName.add("Choose up to one player within 8 blocks of your character. That player may not make any movement during their next turn.");
-    ListName.add("Rat");
-    ListName.add("Choose up to one player within 8 blocks of your character. That player may not make any movement during their next turn.");
-    ListName.add("Rat");
-    ListName.add("Choose up to one player within 8 blocks of your character. That player may not make any movement during their next turn.");
-    ListName.add("Call Train");
-    ListName.add("The train moves to your quadrant.");
-    ListName.add("Call Train");
-    ListName.add("The train moves to your quadrant.");
-    ListName.add("Call Train");
-    ListName.add("The train moves to your quadrant.");
-    ListName.add("Call Train");
-    ListName.add("The train moves to your quadrant.");
-    ListName.add("Call Train");
-    ListName.add("The train moves to your quadrant.");
-    ListName.add("Reason");
-    ListName.add("For this turn, if you do battle with another player, add 2 to the amount you roll.");
-    ListName.add("Reason");
-    ListName.add("For this turn, if you do battle with another player, add 2 to the amount you roll.");
-    ListName.add("Reason");
-    ListName.add("For this turn, if you do battle with another player, add 2 to the amount you roll.");
-    ListName.add("Reset");
-    ListName.add("All players move their characters back to their spots at the start of the game.");
-    ListName.add("Reset");
-    ListName.add("All players move their characters back to their spots at the start of the game.");
-    ListName.add("Reset");
-    ListName.add("All players move their characters back to their spots at the start of the game.");
-    ListName.add("Bullet Proof");
-    ListName.add("When your character is attacked by another character, discard this card immediately. No battle is conducted and both characters remain on the same square.");
-    ListName.add("Bullet Proof");
-    ListName.add("When your character is attacked by another character, discard this card immediately. No battle is conducted and both characters remain on the same square.");
-    ListName.add("Bullet Proof");
-    ListName.add("When your character is attacked by another character, discard this card immediately. No battle is conducted and both characters remain on the same square.");
-    ListName.add("Fisher Yates");
-    ListName.add("All players shuffle their hands and the discard pile back into the deck.");
-    ListName.add("Fisher Yates");
-    ListName.add("All players shuffle their hands and the discard pile back into the deck.");
-    ListName.add("Fisher Yates");
-    ListName.add("All players shuffle their hands and the discard pile back into the deck.");
-    ListName.add("Mr. Ng");
-    ListName.add("When a battle is about to be conducted during any player's turn, you may discard this card and end the battle. The characters that were about to conduct battle stay on the same spot.");
-    ListName.add("Mr. Ng");
-    ListName.add("When a battle is about to be conducted during any player's turn, you may discard this card and end the battle. The characters that were about to conduct battle stay on the same spot.");
-    ListName.add("Mr. Ng");
-    ListName.add("When a battle is about to be conducted during any player's turn, you may discard this card and end the battle. The characters that were about to conduct battle stay on the same spot.");
-    ListName.add("Hyper Card");
-    ListName.add("Randomly select 1 card in the discard pile and add it to your hand.");
-    ListName.add("Hyper Card");
-    ListName.add("Randomly select 1 card in the discard pile and add it to your hand.");
-    ListName.add("Hyper Card");
-    ListName.add("Randomly select 1 card in the discard pile and add it to your hand.");
-    ListName.add("CIC");
-    ListName.add("Choose one of the other players and look at his or her hand. Select one card and take it from that player.");
-    ListName.add("CIC");
-    ListName.add("Choose one of the other players and look at his or her hand. Select one card and take it from that player.");
-    ListName.add("CIC");
-    ListName.add("Choose one of the other players and look at his or her hand. Select one card and take it from that player.");
-    ListName.add("FED");
-    ListName.add("Choose one of the other players and take one random card from his or her hand.");
-    ListName.add("FED");
-    ListName.add("Choose one of the other players and take one random card from his or her hand.");
-    ListName.add("FED");
-    ListName.add("Choose one of the other players and take one random card from his or her hand.");
-
-
-
-
-
-
-             
+    ListName.add("Kevin");
+    ListName.add("Simon");
+    ListName.add("Faggot");
+    ListName.add("Nib");
+    ListName.add("Victor");
   }
   
   public  void Program(){
-    int ListSize = (ListName.size() / 2);
+    int ListSize = ListName.size();
     if (ListSize != 0){
       int R = (int)(Math.random() * 100000) + 1;
       while (R > ListSize){
         R = (int)(Math.random() * 100000) + 1;
       }
-      System.out.println(ListName.get((R * 2) - 2));
-      ListName.remove((R * 2) - 2);
-      System.out.println(ListName.get((R * 2) - 2));
-      ListName.remove((R * 2) - 2);
+      //System.out.println(R);
+      System.out.println(ListName.get(R - 1));
+      ListName.remove(R - 1);
+      //System.out.println(ListName);
       ListSize = ListName.size();
+      //System.out.println(ListSize);
+      // System.out.println("debug");
     }
     if (ListSize == 0){
       Breaker = 1;
